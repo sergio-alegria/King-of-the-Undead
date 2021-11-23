a097b9176e8f0d00e9514d81a7f111e097fb0fb9
raule6 <eguren71@gmail.com>
2021-11-23 15:43:32 +0100

    Character Definition

--- Character.py
+++ Character.py
@@ -1,32 +1,70 @@
 from abc import ABC, abstractmethod
+from common import Dir
 
 class Character(ABC):
-     
-    def __init__(self, x : int):
-        self.x = x
-        pass
+
+    def __init__(self, id, hp, pos, dir, dmg, sprites):
+        self.id = id
+        self.hp = hp
+        self.pos = pos
+        self.dir = Dir.stall
+        self.dmg = dmg
+        self.sprites = sprites
 
     def draw(self) -> None:
         pass
 
-    def update() -> None:
-        pass
+    def update(self) -> None:
+        match self.dir:
+            case Dir.up :
+                self.pos.y += 1
+            case Dir.down :
+                self.pos.y -= 1
+            case Dir.left :
+                self.pos.x -= 1
+            case Dir.right :
+                self.pos.x += 1
     
-    def move() -> None:
-        pass
+    def move(self, dir) -> None:
+        self.dir = dir
     
-    def attack() -> None:
-        pass
+    def receiveDmg(self, dmg = 1) -> None:
+        self.hp -= dmg
+        if self.hp <= 0:
+            self.die()
+
+    def attack(self) -> None:
+        return self.dir, self.dmg
     
+    @abstractmethod
     def die() -> None:
         pass
 
+
+class MainCharacter(Character):
+
+    def die() -> None:
+        #Fin del juego
+        pass
+
+
 class Enemy(Character):
-    def __init__(self, id, x : int = None):
-        super().__init__(x)
+
+    def __init__(self, id, hp, pos, dir, sprites):
+        super().__init__(self, id, hp, pos, dir, sprites)
     
     def draw(self) -> None:
         print(self.x)
 
-    
-    
+
+class Point():
+
+    def __init__(self, x : int = None, y : int = None):
+        self.x = x
+        self.y = y
+
+
+class Weapon(ABC):
+
+    def __init__(self, dmg):
+        self.dmg = dmg
---
+++ common.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class Dir(Enum):
+    up = 0
+    down = 1
+    left = 2
+    right = 3
+    stall = 4
