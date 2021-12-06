8decc089840be880454389ed1b8a5427ae805d03
raule6 <eguren71@gmail.com>
2021-12-06 16:32:09 +0100

    Character 2

--- Character.py
+++ Character.py
@@ -1,35 +1,34 @@
-from abc import ABC, abstractmethod
 import common
 import pygame
 
 #store character images un a list
-character_dict = {"Top_walk" : [],
-                  "Top_walk_S" : [],
-                  "Down_walk" : [],
-                  "Down_walk_S" : [],
-                  "Rside_walk" : [],
-                  "Rside_walk_S" : [],
-                  "Lside_walk" : [],
-                  "Lside_walk_S" : [],
-                  "Top_attack" : [],
-                  "Down_attack" : [],
-                  "Lside_attack" : [],
-                  "Rside_attack" : [],
-                  "Dying" : []}
+main_character_animations = {"Top_walk" : [],
+                             "Top_walk_S" : [],
+                             "Down_walk" : [],
+                             "Down_walk_S" : [],
+                             "Rside_walk" : [],
+                             "Rside_walk_S" : [],
+                             "Lside_walk" : [],
+                             "Lside_walk_S" : [],
+                             "Top_attack" : [],
+                             "Down_attack" : [],
+                             "Lside_attack" : [],
+                             "Rside_attack" : [],
+                             "Dying" : []}
 
 character_list = (8, 8, 8, 8, 8, 12, 0, 8, 6, 6, 6, 6, 4)
 
-for i in character_dict:
+for i in main_character_animations:
     for j in character_list:
         for k in range(j):
             img = pygame.image.load(f'resources/Sprites pj/{i}/{i}-{k + 1}.png').convert_alpha()
             img = pygame.transform.scale(img, (common.TILE_SIZE, common.TILE_SIZE))
-            character_dict[i] = img
+            main_character_animations[i] = img
             break
     break
 
-class Character(ABC):
-    def __init__(self, id, hp, pos, dir, dmg, sprites):
+class Character():
+    def __init__(self, id, hp, pos, dir, dmg, sprites, index, image):
         """
             Init class for the character
             
@@ -41,11 +40,8 @@ class Character(ABC):
         self.pos = pos
         self.dir = common.Dir.stall
         self.dmg = dmg
-        self.sprites = sprites
-
-    def draw(self) -> None:
-        #screen.blit(self.image, self.rect)
-        pass
+        self.sprites = main_character_animations
+        self.image = image
 
     def update(self) -> None:
         if self.dir == common.Dir.up:
@@ -81,11 +77,11 @@ class Character(ABC):
     def attack(self) -> None:
         return self.dir, self.dmg
 
-    @abstractmethod
     def die() -> None:
         pass
 
 
+'''
 class MainCharacter(Character):
 
     def __init__(self, id, hp, pos, dir, dmg, sprites):
@@ -120,3 +116,4 @@ class Weapon(ABC):
 
     def __init__(self, dmg):
         self.dmg = dmg
+'''
