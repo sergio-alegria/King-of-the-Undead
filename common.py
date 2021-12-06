a7fb557e27ffbfd6ae27deb47bd595331f262464
Alejandro <80407863+benited44@users.noreply.github.com>
2021-12-06 16:34:56 +0100

    cambios common

--- common.py
+++ common.py
@@ -14,7 +14,7 @@ speed = 3
 
 # Key = backforund image, values = tile that need that bg
 NEED_BAKGROUND = {
-    0 : [1,2,3]
+    0 : [15, 16, 17, 18, 19, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 38]
 }
 
 class Dir(Enum):
