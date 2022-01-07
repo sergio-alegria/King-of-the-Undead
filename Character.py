833769bd6fac91ae8eecdc09b37ca4498a58e352
raule6 <eguren71@gmail.com>
2022-01-07 15:56:41 +0100

    Half A*

--- Character.py
+++ Character.py
@@ -1,44 +1,39 @@
 import pygame
 import common
-import random 
+import random
 import a_star_matrix
 import numpy
+from common import Point
 
 
-class Point():
-    def __init__(self, x : int = None, y : int = None):
-        self.x = x
-        self.y = y
-        
-    def toTuple(self):
-        return (self.x,self.y)
-
-class Weapon():
+class Weapon:
     def __init__(self, dmg, range):
         self.dmg = dmg
         self.range = range
-    
+
     def get_rect(self, pos, direction):
         if dir == common.Dir.up or dir == common.Dir.down:
-            return pygame.Rect(pos,(self.range/2, self.range))     
-        else: 
-            return pygame.Rect(pos,(self.range, self.range/2))
-        
-class Character():
+            return pygame.Rect(pos, (self.range / 2, self.range))
+        else:
+            return pygame.Rect(pos, (self.range, self.range / 2))
+
+
+class Character:
     ATTACK_FRAMES = 6
+
     def __init__(self, id, hp, pos, sprites, starting_sprite="Stall"):
         """
-            Init class for the character
-            
-            :param id: Character's id
-            :type id: int
+        Init class for the character
+
+        :param id: Character's id
+        :type id: int
         """
         self.id = id
         self.hp = hp
         self.pos = Point(pos[0], pos[1])
         self.weapon = Weapon(0.5, 20)
         self.attacking_frames = 0
-        self.attacking_counter = 0 
+        self.attacking_counter = 0
         self.sprites = sprites
         self.sprite_key = starting_sprite
         self.img_index = 0
@@ -53,19 +48,23 @@ class Character():
             self.is_attacking = False
             pass
         try:
-            if self.is_moving: 
-                self.img_index = (self.img_index + 1) % len(self.sprites[self.sprite_key])
-        except IndexError: pass
+            if self.is_moving:
+                self.img_index = (self.img_index + 1) % len(
+                    self.sprites[self.sprite_key]
+                )
+        except IndexError:
+            pass
         finally:
             self.image = self.sprites[self.sprite_key][self.img_index]
-    
+
     def change_sprite(self, sprite_key):
-        if self.is_attacking: return # If attacking dont change
+        if self.is_attacking:
+            return  # If attacking dont change
         self.sprite_key = sprite_key
-        
+
     def move(self, dir) -> None:
         if self.attacking_frames:
-            pass 
+            pass
         if dir == self.dir:
             return
         self.is_moving = True
@@ -87,54 +86,45 @@ class Character():
     def receive_dmg(self, dmg) -> bool:
         self.hp -= dmg
         if self.hp <= 0:
-           return True
+            return True
         return False
-         
+
     def attack(self):
         direction = self.sprite_key.split("_")[0]
-        if direction == 'Stall':
-            direction = 'Down'
+        if direction == "Stall":
+            direction = "Down"
         self.prev_key = self.sprite_key
         self.change_sprite(f"{direction}_attack")
         self.is_atacking = True
         return self.dir, self.weapon.dmg
-        
+
     def die(self) -> bool:
         self.died_counter = 1
         self.change_sprite("Dying")
-    
+
     def AI_move(self, mc_pos):
         self.is_moving = True
         moves = [common.Dir.stall]
         if self.pos.x > mc_pos.x:
-           moves.append(common.Dir.left) 
+            moves.append(common.Dir.left)
         elif self.pos.x < mc_pos.x:
             moves.append(common.Dir.right)
         if self.pos.y < mc_pos.y:
             moves.append(common.Dir.up)
         if self.pos.y > mc_pos.y:
             moves.append(common.Dir.down)
-        #print(f"{self.id}\nenemy(x,y)=({self.pos.x},{self.pos.y})\nmain_char(x,y)=({mc_pos.x},{mc_pos.y})\n{moves = }")    
-        self.move(moves[random.randint(0,len(moves)-1)])
+        # print(f"{self.id}\nenemy(x,y)=({self.pos.x},{self.pos.y})\nmain_char(x,y)=({mc_pos.x},{mc_pos.y})\n{moves = }")
+        self.move(moves[random.randint(0, len(moves) - 1)])
 
-    def AI_move_a_star(self):
+    def AI_move_a_star(self, map, pos: Point):
         self.is_moving = True
         moves = [common.Dir.stall]
-        nmap = numpy.array([
-                           [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                           [1,1,1,1,1,1,1,1,1,1,1,1,0,1],
-                           [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                           [1,0,1,1,1,1,1,1,1,1,1,1,1,1],
-                           [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                           [1,1,1,1,1,1,1,1,1,1,1,1,0,1],
-                           [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                           [1,0,1,1,1,1,1,1,1,1,1,1,1,1],
-                           [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                           [1,1,1,1,1,1,1,1,1,1,1,1,0,1],
-                           [0,0,0,0,0,0,0,0,0,0,0,0,0,0]])
-        print(a_star_matrix.astar(nmap, (0,0), (10,13)))
+        nmap = numpy.array(map)
+        print(map)
+        print(a_star_matrix.astar(nmap, self.pos.toTuple(), pos.toTuple()))
 
-'''
+
+"""
 class MainCharacter(Character):
 
     def __init__(self, id, hp, pos, dir, dmg, sprites):
@@ -165,4 +155,4 @@ class Weapon(ABC):
 
     def __init__(self, dmg):
         self.dmg = dmg
-'''
+"""
--- Map.py
+++ Map.py
@@ -1,70 +1,69 @@
 from pathlib import Path
 import csv
 import common
-from Character import Point
 
 DEBUG_ROW = 5
-DEBUG = [1]   
+DEBUG = [1]
 
-class Map():
+
+class Map:
     def __init__(self, level):
         self.level = level
         self.grid = []
         self.load_from_csv()
-        
-        print(f'{self.grid = }')
-        
+
     def load_from_csv(self):
         path = Path("levels")
         path = path / f"level{self.level}_data.csv"
-        #print(f'{path = }')
-        with open(path, newline='') as csvfile:
-            reader = csv.reader(csvfile, delimiter=',')
+        # print(f'{path = }')
+        with open(path, newline="") as csvfile:
+            reader = csv.reader(csvfile, delimiter=",")
             for row in reader:
                 self.grid.append([int(e) for e in row])
-               
-                
+
     def diplayable_sub_matrix(self, x, y):
         matrix = []
-        
+
         # Index calculation
-        offset_up = y-common.DISPLAY_ROWS//2
-        offset_down = y+common.DISPLAY_ROWS//2
-        offset_left = x-common.DISPLAY_COLS//2
-        offset_right = x+common.DISPLAY_COLS//2
-        
+        offset_up = y - common.DISPLAY_ROWS // 2
+        offset_down = y + common.DISPLAY_ROWS // 2
+        offset_left = x - common.DISPLAY_COLS // 2
+        offset_right = x + common.DISPLAY_COLS // 2
+
         if offset_up < 0:
-            offset_down -= offset_up # If y - is < 0 then add those rows to the other side
+            offset_down -= (
+                offset_up  # If y - is < 0 then add those rows to the other side
+            )
             offset_up = 0
         elif offset_down > common.ROWS:
             offset_up -= offset_down - common.ROWS
             offset_down = common.ROWS
-        
+
         if offset_left < 0:
             offset_right -= offset_left
             offset_left = 0
         elif offset_right > common.MAX_COLS:
             offset_left -= offset_right - common.MAX_COLS
-            offset_right = common.MAX_COLS 
-        
+            offset_right = common.MAX_COLS
+
         print(f"{offset_up   = }  {offset_down  = }")
         print(f"{offset_left = }  {offset_right = }")
         for row in self.grid[offset_up:offset_down]:
             matrix.append(row[offset_left:offset_right])
         return matrix
-        
-    def getTile(self, pos: Point):
+
+    def getTile(self, pos: common.Point):
         factor = common.TILE_SIZE
         print(pos.__dict__)
-        print(f'{pos.x//factor}')
-        print(f'{pos.y//factor}')
-        print(self.grid[pos.x//factor][pos.y//factor])
-        return self.grid[pos.x//factor][pos.y//factor]    
+        print(f"{pos.x//factor}")
+        print(f"{pos.y//factor}")
+        print(self.grid[pos.x // factor][pos.y // factor])
+        return self.grid[pos.x // factor][pos.y // factor]
 
     def parse_map(self):
-        map_parsed = [[0 if e not in common.WALL_TILES else 1 for e in row] for row in self.grid]
-        return map_parsed
-
+        self.map_parsed = [
+            [0 if e not in common.WALL_TILES else 1 for e in row] for row in self.grid
+        ]
 
 
 def debug_grid(map):
@@ -76,9 +75,4 @@ if __name__ == "__main__":
     m = Map(0)
     with open("parsed.csv", "w") as f:
         for row in m.parse_map():
-            f.write(str(row) + '\n')
-    
-        
-    
-        
-        
+            f.write(str(row) + "\n")
